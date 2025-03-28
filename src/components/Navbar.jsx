e69d2a42f1ca760a4b19903657248d66ee6c0ba3
BabulHoda98 <babulhoda98@gmail.com>
2025-03-28 23:46:18 +0530

    components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <h3>Logo</h3>
+        <ul>
+            <li NavLink to="">Home</li>
+        </ul>
+        <ul>
+            <li NavLink to="">About</li>
+        </ul>
+        <ul>
+            <li NavLink to="">Contact</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Navbar
