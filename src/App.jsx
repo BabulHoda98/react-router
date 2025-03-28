af17bc9a5f5675b6f7ba3b72654cc7cb930696e4
BabulHoda98 <babulhoda98@gmail.com>
2025-03-28 23:56:55 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>
+      <Footer/>
     </>
   );
 };
